dfc45473513a08037c096eac2e697bb90520b538
aljaztravnik <travnik.aljaz@gmail.com>
2024-01-09 00:43:03 +0100

    neki za slike

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f270c46a382aa1d9a1acd6e18ab098cbe5d93ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be934cbb71ea0fa06040eaaea32e3a8132f0a337")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
